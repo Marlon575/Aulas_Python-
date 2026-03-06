1382456c13cd9e11aaf9c23b84e40c125cc49c89
Marlon <marlonmuchanga@gmail.com>
2026-03-06 21:20:26 -0100

    Aula_31 concluída

---
+++ Aula_31/01_Encontrar_maior_e_menor_número_mesmo_programa.py
@@ -0,0 +1,17 @@
+lista = []
+for i in range (5):
+    numero = int(input("Digite os números: "))
+    lista.append(numero)
+
+maior = lista[0]
+menor = lista[0]
+
+for numero in lista:
+    if numero > maior:# Se encontrar um número da lista, maior que o primeiro número da lista.
+        maior = numero # Maior passa a ser esse número encontrado.
+    
+    if numero < menor: # Se encontrar um número ds lista, menor que o primeiro número da lista.
+        menor = numero # Menor passa a ser esse número encontrsdo.
+
+print("Maior número:  ", maior)
+print("Menor número:  ", menor)
